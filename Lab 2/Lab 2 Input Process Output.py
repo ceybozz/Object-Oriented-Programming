4e4cc429b2ea0dc90cb094f9c0d76b9d4f2e8806
ceybozz <91478796+ceybozz@users.noreply.github.com>
2022-07-06 19:25:53 +0200

    Add files via upload

---
+++ Lab 2/Lab 2 Input Process Output.py
@@ -0,0 +1,5 @@
+miles = float(input('Type in miles: ')) #Variabel 1 
+liters = float(input('Type im liters: '))   #Variabel 2 
+milesPerLiters = miles / liters #Variabel 3 
+
+print("The car goes on " + str(milesPerLiters) + " liter/mile.") #Skriver ut variabel 3
